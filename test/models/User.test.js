42c567ca2195720836e4661c25c67fc95567bb47
Ivan <i.poblete915@gmail.com>
2019-02-04 10:22:53 -0800

    started testing User model

---
+++ test/models/User.test.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const User = require('../../lib/models/User');
+const mongoose = require('mongoose');
+const { Types } = require('mongoose');
+
+describe('User models', () => {
+
+  beforeEach(done => {
+    mongoose.connection.dropDatabase(done);
+  });
+  
+  afterAll(done => {
+    mongoose.connection.close(done);
+  });
+
+  it('validates a good model', () => {
+    return User.create({
+      email: 'test@test.com',
+      password: 'abc123',
+      role: 'chef'
+    })
+      .then(user => {
+        console.log('user!', user);
+        expect(user).toEqual({
+          email: 'test@test.com',
+          password: 'abc123',
+          role: expect.any(String),
+          _id: expect.any(Types.ObjectId)
+        });
+      });
+  });
+});
