1b0ae62483a3cc2325b8e08cc0b1b50a27ac2adb
Carmen Perezchica <crperezchica@gmail.com>
2019-02-06 13:54:33 -0800

    Ivan commit passing creates an attendee test

--- test/routes/attendees.test.js
+++ test/routes/attendees.test.js
@@ -2,29 +2,35 @@
 // require('../../lib/utils/connect')();
 const request = require('supertest');
 const app = require('../../lib/app');
-const { getToken, getAttendee } = require('../dataHelpers');
-const { Types } = require('mongoose');
+const { getToken, getPopUp, getUser } = require('../dataHelpers');
+// const { Types } = require('mongoose');
 
 describe('attendee model', () => {
   it('creates an attendee', () => {
-    return getAttendee()
-      .post('/attendees')
-      .set('Authorization', `Bearer ${getToken()}`)
-      .send({
-        user: 'visitor',
-        popUp: 'Junjun',
-        partySize: 2
-      })
-      .then(res => {
-        console.log(res.body);
-        expect(res.body).toEqual({
-          _id: expect.any(String),
-          user: expect.any(String),
-          popUp: expect.any(Types.ObjectId),
-          partySize: expect.any(Number),
-          __v: 0
-        });
+    return getPopUp()
+      .then(popUp => {
+        return getUser()
+          .then(user => {
+            return request(app)
+              .post('/attendees')
+              .set('Authorization', `Bearer ${getToken()}`)
+              .send({
+                user: user._id,
+                popUp: popUp._id,
+                partySize: 2
+              });
+
+          })
+          .then(res => {
+            console.log('RES', res.body);
+            expect(res.body).toEqual({
+              __v: 0,
+              _id: expect.any(String),
+              user: expect.any(String),
+              popUp: expect.any(String),
+              partySize: 2
+            });
+          });
       });
   });
-})
-;
+});
