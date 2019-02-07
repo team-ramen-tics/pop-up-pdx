1f787d8a31e2e6f948db4748f78706e6763e9f39
Dyanna Tolman <dyannast@gmail.com>
2019-02-06 23:21:01 -0800

    increased attendees for a pop up

--- test/seedData.js
+++ test/seedData.js
@@ -5,10 +5,10 @@ const Chance = require('chance');
 const chance = new Chance();
 const Attendee = require('../lib/models/Attendee');
 
-const DEFAULT_TOTAL_VISITORS = 60;
+const DEFAULT_TOTAL_VISITORS = 50;
 const DEFAULT_TOTAL_CHEFS = 10;
 const DEFAULT_TOTAL_POPUPS = 10;
-const DEFAULT_TOTAL_ATTENDEES = 50;
+const DEFAULT_TOTAL_ATTENDEES = 40;
 
 module.exports = async({
   totalVisitors = DEFAULT_TOTAL_VISITORS,
@@ -39,6 +39,6 @@ module.exports = async({
   await Attendee.create([...Array(totalAttendees)].map(() => {
     const user = chance.pick(visitors)._id;
     const popUp = chance.pickone(popUps)._id;
-    return { partySize: chance.integer({ min: 2, max: 10 }), popUp, user };
+    return { partySize: chance.integer({ min: 2, max: 5 }), popUp, user };
   }));
 };
