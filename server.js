105d19f62c70d79d14f90013e0e3552438ff9f7f
Ivan <i.poblete915@gmail.com>
2019-02-07 09:47:18 -0800

    first deploy

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 require('dotenv').config();
 require('./lib/utils/connect')();
-
 const app = require('./lib/app');
+app.use(express.static('public'));
 
 app.listen(7890, () => {
   console.log('Running on 7890');
